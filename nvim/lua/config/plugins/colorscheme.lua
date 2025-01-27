97d7c6c58d8f8ff8d2cb04775daa168973bc39b2
leonardodaiki <leonardodaiki1@gmail.com>
2025-01-27 00:18:27 -0300

    no idea

--- nvim/lua/config/plugins/colorscheme.lua
+++ nvim/lua/config/plugins/colorscheme.lua
@@ -10,6 +10,7 @@ return {
         "rebelot/kanagawa.nvim",
         config = function()
             vim.cmd.colorscheme('kanagawa-dragon')
+            vim.api.nvim_set_hl(0, "Normal", { bg = '#181616' })
         end
     },
 }
